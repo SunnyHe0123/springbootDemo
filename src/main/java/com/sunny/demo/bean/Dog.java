39f0047c79edabf3f87bc03741f439864d1294a4
DreamLi <>
2021-03-02 23:20:21 +0800

    add ConfigurationProperties

---
+++ src/main/java/com/sunny/demo/bean/Dog.java
@@ -0,0 +1,31 @@
+package com.sunny.demo.bean;
+
+public class Dog {
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public Integer getAge() {
+      return age;
+   }
+
+   public void setAge(Integer age) {
+      this.age = age;
+   }
+
+   @Override
+   public String toString() {
+      return "Dog{" +
+         "name='" + name + '\'' +
+         ", age=" + age +
+         '}';
+   }
+
+   private String name;
+   private Integer age;
+
+}
--- src/test/java/com/sunny/demo/DemoApplicationTests.java
+++ src/test/java/com/sunny/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.sunny.demo;
 
+import com.sunny.demo.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * SpringBoot单元测试
+ * 可以在测试期间很方便的类似编码一样进行自动注入等容器的功能
+ */
 @SpringBootTest
 class DemoApplicationTests {
+   @Autowired
+   Person person;
 
    @Test
    void contextLoads() {
+      System.out.println(person);
    }
 
 }
